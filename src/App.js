7f5c5e84ffc132682c6c386a4ae4e6989994c8bb
A-Abdeen <ahmed.yahya18@gmail.com>
2021-01-21 00:53:46 +0300

    essential styles added

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ function App() {
     return (
       <div>
         <h2>{product.name}</h2>
-        <p>Starting at {product.price} USD</p>
-        <img alt={product.name} src={product.image}></img>
+        <p style={styles.list}>Starting at {product.price} USD</p>
+        <img
+          styles={style.prodImg}
+          alt={product.name}
+          src={product.image}
+        ></img>
       </div>
     );
   });
@@ -17,7 +21,11 @@ function App() {
     <div className="App-header">
       <h1>Rust Shop</h1>
       <h4>Everything is worthless, we still make money</h4>
-      <img alt="Rust Bros shop sign" src="./RustBros.jpg"></img>
+      <img
+        style={styles.shopImg}
+        alt="Rust Bros shop sign"
+        src="./RustBros.jpg"
+      ></img>
     </div>
   );
 }
--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,17 @@
-const styles = {};
+const styles = {
+  prodImg: { width: "200px", height: "200px" },
+  list: {
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+  },
+  shopImg: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "100%",
+  },
+  text: { textAlign: "center" },
+};
 
 export default styles;
