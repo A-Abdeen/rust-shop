d8273d24c90517aeffa5abb6d15b000fc5078f1d
A-Abdeen <ahmed.yahya18@gmail.com>
2021-01-20 20:16:44 +0300

    Complete app minus styles object prior to first React attempt

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
+import products from "./products.js";
+import styles from "./styles.js";
 
 function App() {
+  const productsList = products.map((product) => {
+    return (
+      <div>
+        <h2>{product.name}</h2>
+        <p>Starting at {product.price} USD</p>
+        <img alt={product.name} src={product.image}></img>
+      </div>
+    );
+  });
+
   return (
     <div className="App-header">
       <h1>Rust Shop</h1>
       <h4>Everything is worthless, we still make money</h4>
-      <img src="./RustShop.jpg"></img>
+      <img alt="Rust Bros shop sign" src="./RustBros.jpg"></img>
     </div>
   );
 }
-
 export default App;
---
+++ src/products.js
@@ -0,0 +1,14 @@
+const products = [
+  {
+    name: "1969 Dodge Super Bee A12 2 DR Coupe",
+    price: 125000,
+    image: "./1969_Dodge_Super_Bee_A12_2_DR_Coupe.jpg",
+  },
+  {
+    name: "1973 Ford Falcon XA GT",
+    price: 77400,
+    image: "./1973_Ford_Falcon_XA_GT.jpg",
+  },
+];
+
+export default products;
