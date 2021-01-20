c4ace73c01ee9b38090df7286cb830fc6e2ce1e5
A-Abdeen <ahmed.yahya18@gmail.com>
2021-01-20 15:36:11 +0300

    added product list.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <h1>Rust Shop</h1>
+      <h4>Everything is worthless, we still make money</h4>
+      <img src="./RustShop.jpg"></img>
     </div>
   );
 }
