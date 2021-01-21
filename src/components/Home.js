81e7dbfd2e6248f22dc755bcfaf2741c627709bd
A-Abdeen <ahmed.yahya18@gmail.com>
2021-01-21 11:33:44 +0300

    added components and finished home component

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import { findAllByTitle } from "@testing-library/react"
+import "./App.css";
+import styles from "./styles.js";
+
+const Home = () => {
+    const shopImg = "./RustBros.jpg",
+    const shopName = "Rust Shop"
+    const shopDescription = "Everything is worthless, we still make money"
+    ;
+    return (
+        <div className="App-header">
+          <h1 style={styles.text}> {shopName} </h1>
+          <h4 style={styles.text}> {shopDescription} </h4>
+          <img
+            style={styles.shopImg}
+            alt="Rust Bros shop sign"
+            
+          >{shopImg}</img>
+        </div>
+      );
+    }
+export default Home
