eaf2225b920c83d81edb9bf2cc3940ed14359853
A-Abdeen <ahmed.yahya18@gmail.com>
2021-01-26 09:26:14 +0300

    components and props

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,1 +1,17 @@
-// props.x.name;
+import React from "react";
+import "../App.css";
+import styles from "../styles.js";
+
+const Car = props => {
+    const car = props.car;
+    return (
+        <div style={styles.car} key={car.id}>
+            <img style={styles.carImg} alt={car.name} src={car.image} />
+            <p style={styles.text}>{car.name}</p>
+            <p style={styles.text}>{car.price} USD</p>
+        </div>
+      );}
+
+
+
+export default Car;
--- src/components/CarYard.js
+++ src/components/CarYard.js
@@ -1,37 +1,12 @@
 import React from "react";
 import "../App.css";
 import styles from "../styles.js";
+import Car from "./Car";
 import cars from "../cars.js";
 
-const CarYard = () => {
-const carYard = cars.map((car) =>  (
-  <div key={car.id}>
-    <img style={styles.carImg} alt={car.name} src={car.image} />
-    <p style={styles.text}>{car.name}</p>
-    <p style={styles.text}>{car.price} USD</p>
-  </div>
-));
-return <div style={styles.list}>{carYard}</div>;
-}
+const CarYard = (props) => {
+  const carYard = cars.map((car) => <Car car={car} key={car.id} />);
+  return <div style={styles.list}>{carYard}</div>;
+};
 
 export default CarYard;
-    
-    // return (
-    //   <div style={styles.list}>
-    //     <img
-    //       style={styles.carImg}
-    //       alt={car.name}
-    //       src={car.image}
-    //       ></img>
-        
-    //     <p>{car.price} USD</p>
-    //   </div>
-
-// const carYard = cars.map((car) => {
-//   return;
-//   <Car x={car} />;
-// });
-      
-
-//   });
-// };
