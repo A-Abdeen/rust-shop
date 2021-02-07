a00304a670c39ba03d667d464e91b53d2104c531
A-Abdeen <ahmed.yahya18@gmail.com>
2021-02-07 16:49:43 +0300

    fixed photos and cars properties

--- src/components/CarDetail.js
+++ src/components/CarDetail.js
@@ -7,17 +7,19 @@ import DeleteButton from "./DeleteButton.js";
 const CarDetail = (props) => {
   const carSlug = useParams().carSlug;
   const car = props.cars.find((car) => car.slug === carSlug);
-  if (!car) return <Redirect to="/car-yard" />;
+  if (!car) return <Redirect to="/cars" />;
 
   return (
     <DetailWrapper>
-      <h1>{car.name}</h1>
-      <img className="img" src={car.image} alt={car.name} />
-      <p>{car.description}</p>
-      <p>${car.price}</p>
       <Link to="/cars">
         <BackButton>Go Back</BackButton>
       </Link>
+      <h1>{car.name}</h1>
+      <h5>Model Year: {car.year}</h5>
+      <h6>Manufacturer: {car.manufacturer}</h6>
+      <img className="img" src={car.image} alt={car.name} />
+      <p>{car.description}</p>
+      <p>${car.price}</p>
       <DeleteButton deleteCar={props.deleteCar} car={car} />
     </DetailWrapper>
   );
